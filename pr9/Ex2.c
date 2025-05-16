5d11e56bcb1a3ebb44460f19485773f9e7e113c1
Anfisa <midlovetsanfisa@gmail.com>
2025-05-16 23:38:34 +0300

    ex2

--- pr9/Ex2.c
+++ pr9/Ex2.c
@@ -3,14 +3,13 @@
 #include <unistd.h>
 
 int main() {
-    // Команда для виконання від імені адміністратора (FreeBSD)
     const char *cmd = "sudo /bin/cat /etc/master.passwd";
 
-    printf("Виконую: %s\n\n", cmd);
+    printf("Perform: %s\n\n", cmd);
     int result = system(cmd);
 
     if (result != 0) {
-        fprintf(stderr, "Не вдалося виконати команду. Перевірте, чи маєте доступ через sudo без пароля.\n");
+        fprintf(stderr, "The team did not go into detail. Make sure you have access via sudo without a password.\n");
         return 1;
     }
 
